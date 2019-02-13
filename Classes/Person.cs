cd16484f8e85d128cdc19a2e9b8c572c03829592
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2019-02-13 17:53:30 +0100

    move Person to class

---
+++ Classes/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public static int _pnr=0;
+        public string Name;
+        public int pnr = ++ _pnr;
+        public void Introduce(string to)
+        {
+
+            Console.WriteLine("PersonsNumber {2} Hi {1}, I am {0} !", to, Name, pnr);
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Classes
 {
-    public class Person
-    {
-        public static int _pnr=0;
-        public string Name;
-        public int pnr = ++ _pnr;
-        public void Introduce(string to)
-        {
-
-            Console.WriteLine("PersonsNumber {2} Hi {1}, I am {0} !", to, Name, pnr);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
